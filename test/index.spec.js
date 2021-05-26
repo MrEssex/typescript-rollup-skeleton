f3c2e5eb05679e89139274675d136e18e63ee73e
Kyle Essex <kylegavinessex@hotmail.com>
2021-05-26 17:30:38 +0100

    Small update

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,19 @@
-import { expect } from 'chai';
-import index from '../dist/index.cjs';
+import {expect} from 'chai';
+import greet from '../dist/index.cjs';
 
 import 'mocha-sinon';
 
-describe('greet()', function () {
+describe('greet()', function ()
+{
   // eslint-disable-next-line mocha/no-hooks-for-single-case
-  beforeEach(function () {
+  beforeEach(function ()
+  {
     this.sinon.stub(console, 'log');
   });
 
-  it('should log "Hello World"', function () {
-    index();
+  it('should log "Hello World"', function ()
+  {
+    greet();
     expect(console.log.calledOnce).to.be.true;
     expect(console.log.calledWith('Hello World')).to.be.true;
   });
